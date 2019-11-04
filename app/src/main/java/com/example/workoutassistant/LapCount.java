c472d9be10e5e1124304b4690a7799eda2639c20
prabh <98prabh@gmail.com>
2019-11-04 22:46:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/workoutassistant/LapCount.java
@@ -0,0 +1,14 @@
+package com.example.workoutassistant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LapCount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lap_count);
+    }
+}
